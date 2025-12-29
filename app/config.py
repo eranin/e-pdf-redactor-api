fe2fcfead2a65bb1942ed6d9763a3a9ac35090c3
Trần tuấn Kiệt <134765192+tuankiet30902@users.noreply.github.com>
2025-12-29 14:34:24 +0700

    update

--- app/config.py
+++ app/config.py
@@ -3,10 +3,10 @@ import os
 STORAGE_DRIVER = os.getenv("STORAGE_DRIVER", "minio")
 
 # MinIO
-MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "localhost")
-MINIO_PORT = os.getenv("MINIO_PORT", "7000")
-MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "admin")
-MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "12345678")
+MINIO_ENDPOINT = os.getenv("MINIO_ENDPOINT", "storage.eranin.com")
+MINIO_PORT = os.getenv("MINIO_PORT", "443")
+MINIO_ACCESS_KEY = os.getenv("MINIO_ACCESS_KEY", "Np2gi4aaWwFspwly")
+MINIO_SECRET_KEY = os.getenv("MINIO_SECRET_KEY", "wucrXoZE5sDOsUMIXqW9zfg1dOw3ID4W")
 MINIO_SECURE = os.getenv("MINIO_SECURE", "false").lower() == "true"
 MINIO_BUCKET_NAME = os.getenv("MINIO_BUCKET_NAME", "asmara-chubbies-tool1")
 
